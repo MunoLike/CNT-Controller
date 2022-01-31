87cccf680434d0665510bd058bbe22c2daf321d0
Munolike <sukorice@gmail.com>
2022-01-31 15:25:16 +0900

    add gui feature(still working)

--- mainCam.py
+++ mainCam.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from datetime import datetime
 import os
+import math
 
 import cvFpsCalc
 import pid
@@ -21,7 +22,7 @@ target_flowrate = 0
 #consts
 W1_NAME = 'test'
 W2_NAME = 'diff'
-TARGET_POS = (152,15,213,76)
+TARGET_POS = (187,10,241,80)
 IS_SIM = True
 INCR_DROP_THRESH = 118
 FIRST_FLOWRATE = 0.8459
@@ -40,6 +41,8 @@ def printCoord(e, x, y,flags, param):
 cv2.namedWindow(W1_NAME, cv2.WINDOW_NORMAL)
 cv2.namedWindow(W2_NAME, cv2.WINDOW_NORMAL)
 cv2.createTrackbar("thresh", W2_NAME, 25, 255, update)
+cv2.createTrackbar("x-orig", W2_NAME, TARGET_POS[0], 640, update)
+cv2.createTrackbar("y-orig", W2_NAME, TARGET_POS[1], 480, update)
 cv2.setMouseCallback(W1_NAME, printCoord)
 
 #setup camera
@@ -48,7 +51,7 @@ if not IS_SIM:
     cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
     cap.set(cv2.CAP_PROP_FPS,120)
-    cap.set(cv2.CAP_PROP_BRIGHTNESS, 60)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS, 50)
 else:
     cap = cv2.VideoCapture('output.avi')
 
@@ -82,7 +85,9 @@ while True:
         
         if not IS_SIM:
             frame=cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
-            frame = frame[TARGET_POS[1]:TARGET_POS[3], TARGET_POS[0]:TARGET_POS[2]]
+            origx = cv2.getTrackbarPos('x-orig', W2_NAME)
+            origy = cv2.getTrackbarPos('y-orig', W2_NAME)
+            frame = frame[origy:origy+(TARGET_POS[3]-TARGET_POS[1]), origx:origx+(TARGET_POS[2]-TARGET_POS[0])]
         
         #if bg is registered
         #calculate diff
@@ -102,15 +107,24 @@ while True:
 
             #if drop is going down
             if in_progress:
-                contours, _ = cv2.findContours(bined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                contours = filter(lambda cnt: cv2.contourArea(cnt) > 5, contours)
-                for i, cnt in enumerate(contours):
-                    ellipse = cv2.fitEllipse(cnt)
-                    isnan = np.isnan(ellipse)
-                    frame = cv2.ellipse(frame, ellipse, (0,84,211), thickness=1)
+#                 contours, _ = cv2.findContours(bined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#                 contours = filter(lambda cnt: cv2.contourArea(cnt) > 5, contours)
+#                 for i, cnt in enumerate(contours):
+#                     ellipse = cv2.fitEllipse(cnt)
+#                     
+#                     x, y = ellipse[0]
+#                     if math.isnan(x) or math.isnan(y):
+#                         continue
+# 
+#                     x, y = ellipse[1]
+#                     if math.isnan(x) or math.isnan(y):
+#                         continue
+# 
+# 
+#                     frame = cv2.ellipse(frame, ellipse, (0,84,211), thickness=1)
 
                 #check whether it dropped
-                if white_sum/present_sum <= 0.1:
+                if white_sum/present_sum <= 0.5:
                     counter += 1
                     in_progress = False
                     if counter == 1:
@@ -136,7 +150,7 @@ while True:
             auto_reset = True
         wait_cnt += 1
         
-        if auto_reset and white_sum <= 20:
+        if auto_reset and white_sum <= 100:
             bg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             print('auto bg reset')
             auto_reset = False
---
+++ tkintertest.py
@@ -0,0 +1,89 @@
+from dis import Instruction
+import tkinter as tk
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master.title("test")
+
+        # app name
+        self.app_title = tk.Label(
+            self.master, text="CNT Film Maker", font=("", 15, "bold")
+        )
+        self.app_title.grid(row=0, column=0, columnspan=3, sticky=tk.N)
+
+        # ratio control describe text
+        self.duty_desctext = tk.Label(
+            self.master, text="Operation Ratio(PWM Duty Ratio)", font=("", 13)
+        )
+        self.duty_desctext.grid(row=1, column=0, sticky=tk.W)
+
+        # Peristaltic Pump PWM ON Ratio
+        self.scale_var = tk.IntVar()
+        self.scaleH = tk.Scale(
+            self.master,
+            variable=self.scale_var,
+            command=self.pwm_slider,
+            orient=tk.HORIZONTAL,
+            length=500,
+            width=20,
+            sliderlength=20,
+            from_=0,
+            to=100,
+            resolution=1,
+            tickinterval=5,
+            takefocus=True,
+        )
+
+        self.scaleH.grid(row=1, column=1, sticky=tk.E + tk.W)
+
+        # duty input box
+        self.duty_box = tk.Entry(width=10, font=("", 20))
+        self.duty_box.insert(tk.END, "0")
+        self.duty_box.bind("<KeyPress>", self.enter_dutybox)
+        vcmd = (self.duty_box.register(self.validation), "%P", "%S", "%d") #validator
+        self.duty_box.configure(validate="key", vcmd=vcmd)
+        self.duty_box.grid(row=1, column=2, sticky=tk.E)
+
+        # raw image view
+
+        # cripped image view
+
+        # differences and binarized image view
+
+        # adjust to window size
+        master.grid_rowconfigure(0, weight=1)
+        master.grid_rowconfigure(1, weight=1)
+
+        master.grid_columnconfigure(0, weight=0)
+        master.grid_columnconfigure(1, weight=1)
+        master.grid_columnconfigure(2, weight=0)
+
+    def pwm_slider(self, e):
+        print(str(self.scale_var.get()))
+
+    def enter_dutybox(self, e):
+        pass
+
+    def validation(self, now_str, insert_str, in_type):
+        print('input',now_str, insert_str)
+        
+        #入力文字が整数か
+        if not insert_str.isdecimal():
+            return False
+
+        #100を超えないようにする
+        if in_type != 0 and int(now_str or '0') > 100:
+            return False
+
+        return True
+
+    def cv_init():
+        pass
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
