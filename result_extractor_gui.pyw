8f41797be93a27531a795e57e06519105fd185f3
MunoLike <sukorice@gmail.com>
2022-02-03 18:01:41 +0900

    create series of result_extractor

---
+++ result_extractor_gui.pyw
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import glob
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+
+
+def dirdiag_clicked():
+    init_dir_path=os.path.abspath((os.path.dirname(__file__)))
+    dir_path=filedialog.askdirectory(initialdir=init_dir_path)
+    dir_path_var.set(dir_path)
+    if dir_path:
+        exec_button.focus_set()
+    
+    
+def execute():
+
+    if not dir_path_var.get():
+        messagebox.showerror('エラー', '処理したいフォルダを選択してください．')
+        return
+    if not messagebox.askyesno('確認', '実行すると元ファイルに変更を加えます．\n初回実行時はバックアップを取ることをおすすめします．\n実行しますか？'):
+        return
+
+    csv_list = glob.glob(dir_path_var.get() + r"/*.csv")
+    print(csv_list)
+
+    for file_path in csv_list:
+        text = ""
+        with open(file_path, "r") as file:
+            body = file.readlines()
+
+            delimiter = body[2][-3]
+
+            if delimiter == ",":
+                continue
+
+            body = body[15 + 20 :]
+            newbody = [''] * len(body)
+
+            for i in range(len(body)):
+                newbody[i] = body[i].split(delimiter)[-1]
+
+            text = "".join(newbody) + "\n"
+
+        with open(file_path, "w") as file:
+            file.write(text)
+
+root = tk.Tk()
+root.title("Result Extractor")
+
+frame1=tk.Frame(root, padx=5, pady=5)
+frame1.grid(row=0, column=0, sticky=tk.EW)
+# ラベル
+dir_label = tk.Label(frame1, text="フォルダ参照")
+dir_label.pack(side=tk.LEFT)
+# フォルダパスインプットボックス
+dir_path_var = tk.StringVar()
+dir_entry = tk.Entry(frame1, textvariable=dir_path_var, width=30)
+dir_entry.pack(side=tk.LEFT, expand=True)
+# 参照ボタン
+dir_button = tk.Button(frame1, text="参照", command=dirdiag_clicked,width=10,height=3)
+dir_button.pack(side=tk.LEFT)
+
+frame2 = tk.Frame(root, padx=5, pady=5) 
+frame2.grid(row=1, column=0, sticky=tk.EW)
+#実行ボタン
+exec_button=tk.Button(frame2, text='卍処理卍', command=execute, height=5)
+exec_button.pack(anchor=tk.CENTER,fill=tk.BOTH)
+
+
+# サイズ変更禁止
+root.resizable(width=False, height=False)
+root.mainloop()
--- tkintertest.py
+++ tkintertest.py
@@ -6,7 +6,7 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
 
-        #variable declartion
+        # variable declartion
         self.registered = False
         self.present_sum = 0
         self.white_sum = 0
@@ -15,8 +15,11 @@ class Application(tk.Frame):
         self.wait_cnt = 0
         self.auto_reset = False
         self.target_flowrate = 0
+        self.is_simulation = False
+        self.video_file_path = 'satellite.mp4'
+        self.cap = None
 
-        #set title
+        # set title
         self.master.title("CNT Film Maker")
 
         # app name
@@ -32,11 +35,11 @@ class Application(tk.Frame):
         self.duty_desctext.grid(row=1, column=0, sticky=tk.W)
 
         # Peristaltic Pump PWM ON Ratio
-        self.scale_var = tk.IntVar()
-        self.scaleH = tk.Scale(
+        self.duty_ratio = tk.IntVar()
+        self.bar_duty_ratio = tk.Scale(
             self.master,
-            variable=self.scale_var,
-            command=self.pwm_slider,
+            variable=self.duty_ratio,
+            command=self.change_pwm_slider,
             orient=tk.HORIZONTAL,
             length=500,
             width=20,
@@ -44,28 +47,87 @@ class Application(tk.Frame):
             from_=0,
             to=100,
             resolution=1,
-            tickinterval=5,
             takefocus=True,
         )
 
-        self.scaleH.grid(row=1, column=1, sticky=tk.E + tk.W)
+        self.bar_duty_ratio.grid(row=1, column=1, sticky=tk.E + tk.W)
 
         # duty input box
-        self.duty_box = tk.Entry(width=10, font=("", 20))
+        self.duty_box = tk.Entry(self.master, width=10, font=("", 20))
         self.duty_box.insert(tk.END, "0")
-        self.duty_box.bind("<KeyPress>", self.enter_dutybox)
-        vcmd = (self.duty_box.register(self.validation), "%P", "%S", "%d") #validator
+        self.duty_box.bind("<KeyPress-Return>", self.press_enter_dutybox)
+        vcmd = (self.duty_box.register(self.validation), "%P", "%S", "%d")  # validator
         self.duty_box.configure(validate="key", vcmd=vcmd)
         self.duty_box.grid(row=1, column=2, sticky=tk.E)
 
         # raw image view
+        self.frame_rawimage = tk.LabelFrame(self.master, text='Raw Image')
+        self.frame_rawimage.grid(row=2, column=0)
+        self.canvas_rawimage = tk.Canvas(self.frame_rawimage)
+        self.canvas_rawimage.grid(row=0, column=1)
 
-        # cripped image view
+        self.crop_posx = tk.IntVar()
+        self.bar_crop_posx = tk.Scale(
+            self.frame_rawimage,
+            variable=self.crop_posx,
+            orient=tk.HORIZONTAL,
+            length=200,
+            width=20,
+            sliderlength=20,
+            from_=0,
+            to=360,
+            resolution=1,
+            takefocus=True,
+        )
+        self.bar_crop_posx.grid(row=1, column=0)
+
+        
+        self.crop_posy = tk.IntVar()
+        self.bar_crop_posy = tk.Scale(
+            self.frame_rawimage,
+            variable=self.crop_posy,
+            orient=tk.VERTICAL,
+            length=200,
+            width=20,
+            sliderlength=20,
+            from_=0,
+            to=360,
+            resolution=1,
+            takefocus=True,
+        )
+        self.bar_crop_posy.grid(row=0, column=1)
+
+
+
+        # cropped image view
+        self.frame_croppedimage = tk.LabelFrame(self.master, text='Cropped Image')
+        self.frame_croppedimage.grid(row=2, column=1)
+        self.canvas_croppedimage = tk.Canvas(self.frame_croppedimage)
+        self.canvas_croppedimage.pack()
 
         # differences and binarized image view
+        self.frame_binarizedimage = tk.LabelFrame(self.master, text='Binarized Image')
+        self.frame_binarizedimage.grid(row=2, column=2)
+        self.canvas_binarizedimage = tk.Canvas(self.frame_binarizedimage)
+        self.canvas_binarizedimage.grid(row=0,column=0)
+
+        self.threshold = tk.IntVar()
+        self.bar_threshold = tk.Scale(
+            self.frame_binarizedimage,
+            variable=self.threshold,
+            orient=tk.HORIZONTAL,
+            length=200,
+            width=20,
+            sliderlength=20,
+            from_=0,
+            to=255,
+            resolution=1,
+            takefocus=True,
+        )
+        self.bar_threshold.grid(row=1, column=0)
 
         # select movie or real cam
-        self.movie_radio = tk.Radiobutton
+        self.movie_radio = tk.Radiobutton()
 
         # adjust to window size
         master.grid_rowconfigure(0, weight=1)
@@ -75,28 +137,40 @@ class Application(tk.Frame):
         master.grid_columnconfigure(1, weight=1)
         master.grid_columnconfigure(2, weight=0)
 
-    def pwm_slider(self, e):
-        #print(str(self.scale_var.get()))
+    def change_pwm_slider(self, e):
+        self.duty_box.delete(0, tk.END)
+        self.duty_box.insert(tk.END, str(self.duty_ratio.get()))
         pass
 
-    def enter_dutybox(self, e):
-        pass
+    def press_enter_dutybox(self, e):
+        self.duty_ratio.set(int(self.duty_box.get()))
 
     def validation(self, now_str, insert_str, in_type):
-        print('input',now_str, insert_str)
-        
-        #入力文字が整数か
+        print('input', now_str, insert_str)
+
+        # 入力文字が整数か
         if not insert_str.isdecimal():
             return False
 
-        #100を超えないようにする
+        # 100を超えないようにする
         if in_type != 0 and int(now_str or '0') > 100:
             return False
 
         return True
 
-    def cv_init():
+    def openfile(self):
         pass
+
+    def cv_init(self):
+        self.cap = None
+        if self.is_simulation:
+            self.cap = cv2.VideoCapture(self.video_file_path)
+        else:
+            self.cap = cv2.VideoCapture(0)
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            self.cap.set(cv2.CAP_PROP_FPS, 120)
+            self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 50)
 
 
 if __name__ == "__main__":
